053bb6b8d4ca67c020f1ef22832888a6e28508ed
ezrafollower <ezrafollower@gmail.com>
2022-09-20 14:54:36 +0800

    add publisher

--- src/healthstatus.py
+++ src/healthstatus.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 
 HealthStatus = [0, 0]
 
-def callback(data):
+def callback_zed(data):
     HealthStatus[0] = 1
     
 def listener():
     rospy.init_node('healthstatus', anonymous=True)
-    rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback)
+    rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback_zed)
+    pub = rospy.Publisher('health/sensortower1', String, queue_size=10)
     while not rospy.is_shutdown():
-        print('Zed : ' + str(HealthStatus[0]) + '       mmwave : ' + str(HealthStatus[1]))
+        healthstring = 'sensortower1@' + str(HealthStatus[0]) + ':' + str(HealthStatus[1])
+        print(healthstring)
+        pub.publish(healthstring)
         HealthStatus[0] = 0
         HealthStatus[1] = 0
-        rospy.sleep(2)
+        rospy.sleep(1)
 
 if __name__ == '__main__':
     print("start health status node\n")
