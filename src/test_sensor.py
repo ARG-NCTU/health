c4aa870d2a1a8e086e1e35762f90e2b742557ae8
ezrafollower <ezrafollower@gmail.com>
2022-09-23 16:46:59 +0800

    update test_sensor.py

--- src/test_sensor.py
+++ src/test_sensor.py
@@ -4,16 +4,21 @@ import pytest
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
+flag = False
 
 def callback_zed(data):
-    assert True
+    if data != NULL:
+        flag = True
 
 def listener():
-    rospy.init_node('sensor_pytest', anonymous=True)
-    rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback_zed)
-    rospy.sleep(5)
-    assert False
+    try:
+        rospy.init_node('sensor_pytest', anonymous=True)
+        rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback_zed)
+        flag = False
+        rospy.sleep(5)
+        assert flag
+    except:
+        assert False
 
 def test_main():
-    print("start health status test node\n")
     listener()
