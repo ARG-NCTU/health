d3965e863d8a8f993773b0aef2d6cc20a67b12db
ezrafollower <ezrafollower@gmail.com>
2022-09-23 17:56:15 +0800

    add mmwave subsribe

--- src/sensortower1.py
+++ src/sensortower1.py
@@ -2,8 +2,12 @@
 import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
+from  ti_mmwave_rospkg.msg import RadarScan
 
 HealthStatus = [0, 0]
+
+def callback_mmwave1(data):
+    HealthStatus[1] = 1
 
 def callback_zed(data):
     HealthStatus[0] = 1
@@ -11,6 +15,7 @@ def callback_zed(data):
 def listener():
     rospy.init_node('sensortower1', anonymous=True)
     rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback_zed)
+    rospy.Subscriber("/sensortower1/radar_2/ti_mmwave/ti_mmwave/radar_scan", RadarScan, callback_mmwave1)
     pub = rospy.Publisher('health/sensortower1', String, queue_size=10)
     while not rospy.is_shutdown():
         healthstring = 'sensortower1@' + str(HealthStatus[0]) + ':' + str(HealthStatus[1])
