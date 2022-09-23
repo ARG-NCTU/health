7fcfebf9dddfc1812631f748f9e0e02f7c44628d
ezrafollower <ezrafollower@gmail.com>
2022-09-23 14:02:36 +0800

    add pytest

---
+++ src/test_sensor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+
+
+def callback_zed(data):
+    assert True
+
+def listener():
+    rospy.init_node('sensor_pytest', anonymous=True)
+    rospy.Subscriber("/zed/zed_node/left_raw/image_raw_color", Image, callback_zed)
+    rospy.sleep(5)
+    assert False
+
+def test_main():
+    print("start health status test node\n")
+    listener()
