cdb9365e79fdf4831b6136e2664d4e77f9840f4c
ezrafollower <ezrafollower@gmail.com>
2022-10-31 17:59:43 +0800

    add pub_to_unity.launch

--- src/pub_to_unity.py
+++ src/pub_to_unity.py
@@ -14,34 +14,19 @@ class image_feature:
         self.image_pub = rospy.Publisher("/dts_health/image_raw/compressed",CompressedImage)
 
     def run(self):
-        im = cv2.imread('/home/ezra/share_data/text.jpg', 1)
-        # convert np image to grayscale
-        featPoints = feat_det.detect(
-            cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
-        time2 = time.time()
-
-        for featpoint in featPoints:
-            x,y = featpoint.pt
-            cv2.circle(image_np,(int(x),int(y)), 3, (0,0,255), -1)
-        
-        cv2.imshow('cv_img', image_np)
-        cv2.waitKey(2)
-
-        #### Create CompressedIamge ####
+        im = cv2.imread('/home/argduckiepond/share_data/text.jpg', 1)
         msg = CompressedImage()
         msg.header.stamp = rospy.Time.now()
         msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        msg.data = np.array(cv2.imencode('.jpg', im)[1]).tostring()
         self.image_pub.publish(msg)
 
 def main(args):
     '''Initializes and cleanup ros node'''
     ic = image_feature()
     rospy.init_node('pub_to_unity', anonymous=True)
-    try:
+    while True:
         ic.run()
-    except KeyboardInterrupt:
-        print "Shutting down pub_to_unity node"
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
