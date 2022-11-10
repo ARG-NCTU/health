5cff7a4f59f01f584332d3eed51e4058ef98f623
ezrafollower <ezrafollower@gmail.com>
2022-11-10 14:25:50 +0800

    add HB and gps and gripper column

--- src/zedtower_good.py
+++ src/zedtower_good.py
@@ -3,20 +3,23 @@ import rospy
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
+sensortowers = ['uav', 'wamv', '_lidartower', '_zedtower_left', '_zedtower_mid', '_zedtower_right']
+columns = ["H.B.","ZEDm","D435","LIDAR","mmWave","GPS","gripper"]
+
 def listener():
     rospy.init_node('zedtower_good', anonymous=True)
-    pub_l = rospy.Publisher('health/zedtower_left', String, queue_size=10)
-    pub_m = rospy.Publisher('health/zedtower_mid', String, queue_size=10)
-    pub_r = rospy.Publisher('health/zedtower_right', String, queue_size=10)
-    pub_lidar = rospy.Publisher('health/lidartower', String, queue_size=10)
+    pub_l = rospy.Publisher('health/_zedtower_left', String, queue_size=10)
+    pub_m = rospy.Publisher('health/_zedtower_mid', String, queue_size=10)
+    pub_r = rospy.Publisher('health/_zedtower_right', String, queue_size=10)
+    pub_lidar = rospy.Publisher('health/_lidartower', String, queue_size=10)
     while not rospy.is_shutdown():
-        healthstring = 'zedtower_left@' + str(1) + ':' + str(4) + ':' + str(0)
+        healthstring = sensortowers[3] + '@1:1:0:0:1:1:0'
         pub_l.publish(healthstring)
-        healthstring = 'zedtower_mid@' + str(1) + ':' + str(4) + ':' + str(0)
+        healthstring = sensortowers[4] + '@1:1:0:0:1:1:0'
         pub_m.publish(healthstring)
-        healthstring = 'zedtower_right@' + str(1) + ':' + str(4) + ':' + str(0)
+        healthstring = sensortowers[5] + '@1:1:0:0:1:1:0'
         pub_r.publish(healthstring)
-        healthstring = 'lidartower@' + str(0) + ':' + str(4) + ':' + str(1)
+        healthstring = sensortowers[2] + '@1:0:0:1:1:1:0'
         pub_lidar.publish(healthstring)
         rospy.sleep(1)
 
